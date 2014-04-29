1df3e1114cee68e08e0491166a37e01a5911f66d
pavel <kalashnikovisme@gmail.com>
2014-04-30 01:46:22 +0400

    good work with all number from 0 to 999

--- lib/kabal/languages/russian.rb
+++ lib/kabal/languages/russian.rb
@@ -13,10 +13,15 @@ class Kabal::Russian < Kabal::Language
   end
 
   def self.two_words(number)
-    names[lang]["two_words"][number / 10] + " " + single(number % 10)
+    number_name = names[lang]["two_words"][number / 10]
+    number_name += " " + single(number % 10) if (number % 10 != 0)
+    number_name
   end
 
-  def self.three_words
-    names[lang]["three_words"][number / 100] + " " + two_words(number % 100)
+  def self.three_words(number)
+    number_name = names[lang]["three_words"][number / 100]
+    return number_name += " " + two_words(number % 100) if (number % 100 >= 20)
+    return number_name += " " + single(number % 100) if (number % 100 != 0)
+    number_name
   end
 end
--- test/lib/kabal_test.rb
+++ test/lib/kabal_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 include Kabal
 
 class KabalTest < TestCase
-  def test_to_text
-    assert_equal Kabal.to_text(99), "один"
+  def test_to_text_with_single_number
+    assert_equal Kabal.to_text(1), "один"
+  end
+  def test_to_text_with_two_words_number
+    assert_equal Kabal.to_text(25), "двадцать пять"
+  end
+  def test_to_text_with_three_words_number
+    assert_equal Kabal.to_text(625), "шестьсот двадцать пять"
   end
 end
