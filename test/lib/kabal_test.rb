60070b5eb2164f644872dc1dede78f30d68d7a11
pavel <kalashnikovisme@gmail.com>
2014-05-01 14:26:38 +0400

    test for Russian

--- test/lib/kabal_test.rb
+++ test/lib/kabal_test.rb
@@ -3,38 +3,12 @@ require 'test_helper'
 include Kabal
 
 class KabalTest < TestCase
-  def test_to_text_with_single_number
-    assert_equal Kabal.to_text(1), "один"
-  end
-  def test_to_text_with_two_words_number
-    assert_equal Kabal.to_text(25), "двадцать пять"
-  end
-  def test_to_text_with_three_words_number
-    assert_equal Kabal.to_text(625), "шестьсот двадцать пять"
-  end
-  def test_to_text_with_ten_powers
-    assert_equal Kabal.to_text(125625), "сто двадцать пять тысяч шестьсот двадцать пять"
-  end
-  def test_to_text_with_ten_powers_millions
-    assert_equal Kabal.to_text(125125625), "сто двадцать пять миллионов сто двадцать пять тысяч шестьсот двадцать пять"
-  end
-  def test_to_text_with_ten_powers_some_number
-    assert_equal Kabal.to_text(1000001), "один миллион один"
-  end
-  def test_to_text_with_google
-    assert_equal Kabal.to_text(10 ** 100), "гугол"
-  end
-  def test_to_text_with_three_words_without_second_order
-    assert_equal Kabal.to_text(101), "сто один"
-  end
-  def test_to_text_with_three_words_without_first_and_second_order
-    assert_equal Kabal.to_text(200), "двести"
-  end
-  def test_to_text_with_thousands
-    assert_equal Kabal.to_text(22000), "двадцать две тысячи"
-  end
   def test_language_init
     Kabal.language = "Russian"
     assert_equal Kabal.current_language, "Russian"
+  end
+
+  def test_to_text
+    assert_equal Kabal.to_text(125125125125), "сто двадцать пять миллиардов сто двадцать пять миллионов сто двадцать пять тысяч сто двадцать пять"
   end
 end
