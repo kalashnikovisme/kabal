c61b6a51f5f0025977537c993e81da43ddeb2160
pavel <kalashnikovisme@gmail.com>
2014-05-01 17:33:54 +0400

    fix bugs

--- Gemfile
+++ Gemfile
@@ -8,4 +8,3 @@ gem 'tconsole', git: "git://github.com/gma/tconsole"
 gem 'coveralls', require: false
 gem 'simplecov'
 gem 'minitest', '4.7.5'
-gem 'russian'
--- lib/kabal/languages/russian/declinations.rb
+++ lib/kabal/languages/russian/declinations.rb
@@ -1,9 +1,21 @@
 class Declinations
   def self.name_with_declination(ten_power_name, count)
     if ten_power_name[-1, 1] == "а"
-      Russian.p(count, ten_power_name, ten_power_name[0..4] + "и", ten_power_name[0..4])
+      return ten_power_name if ends_with_one? count
+      return ten_power_name[0..4] + "и" if end_with_two_or_three_of_four? count
+      ten_power_name[0..4]
     else
-      Russian.p(count, ten_power_name, ten_power_name + "а", ten_power_name + "ов")
+      return ten_power_name if ends_with_one? count
+      return ten_power_name + "а" if end_with_two_or_three_of_four? count
+      ten_power_name + "ов"
     end
+  end
+
+  def self.ends_with_one?(count)
+    count.to_s[-1] == "1"
+  end
+
+  def self.end_with_two_or_three_of_four?(count)
+    count.to_s[-1] == "2" or count.to_s[-1] == "3" or count.to_s[-1] == "4"
   end
 end
