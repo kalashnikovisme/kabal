24dc7780aeb368363454f44cbb4c8795a723a953
pavel <kalashnikovisme@gmail.com>
2014-05-03 04:09:33 +0400

    add supported languages big test

--- test/lib/bigtest.rb
+++ test/lib/bigtest.rb
@@ -4,8 +4,11 @@ include Kabal
 
 class BigTest < TestCase
   def test_all_numbers_for_all_languages
-    (0..10 ** 4).each do |number|
-      assert Kabal.to_text number
+    Kabal.supported_languages.each do |lang|
+      Kabal.language = lang
+      (0..10 ** 4).each do |number|
+        assert Kabal.to_text number
+      end
     end
   end
 end
