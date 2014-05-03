12b783564bf15640c9913ff1652a869a62fb17bb
pavel <kalashnikovisme@gmail.com>
2014-05-03 05:52:48 +0400

    add english class and words from zero to billion

---
+++ lib/kabal/languages/english.rb
@@ -0,0 +1,6 @@
+require 'kabal/languages/language'
+
+class Kabal::English < Kabal::Language
+  def convert(number)
+  end
+end
--- lib/kabal/supported_languages.rb
+++ lib/kabal/supported_languages.rb
@@ -2,5 +2,6 @@ module Kabal
   module SupportedLanguages
     #FIXME add autoload
     require 'kabal/languages/russian'
+    require 'kabal/languages/english'
   end
 end
