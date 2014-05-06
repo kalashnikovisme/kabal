7a149060758d1eacf1d075712d9d46f9f2020801
pavel <kalashnikovisme@gmail.com>
2014-05-06 05:56:02 +0400

    some refactor

--- lib/kabal/languages/russian/fractional_numbers.rb
+++ lib/kabal/languages/russian/fractional_numbers.rb
@@ -8,10 +8,18 @@ module Kabal
       def fractional_ten_powers_name(number)
         ten_powers_name = ""
         if need_pre_word? number
-         ten_powers_name = names[lang]["fractional_pre_words"][fractional_number_order(number) % 3] + names[lang]["fractional_ten_powers"][fractional_number_order(number) - (fractional_number_order(number) % 3)]
+          ten_powers_name = fractional_pre_word(number) + fractional_order_word(number)
         else
           ten_powers_name = names[lang]["fractional_ten_powers"][fractional_number_order(number)]
         end
+      end
+
+      def fractional_order_word(number)
+        names[lang]["fractional_ten_powers"][fractional_number_order(number) - (fractional_number_order(number) % 3)]
+      end
+
+      def fractional_pre_word(number)
+        names[lang]["fractional_pre_words"][fractional_number_order(number) % 3]
       end
 
       def need_pre_word?(number)
@@ -23,8 +31,12 @@ module Kabal
       end
 
       def fractional_part_name(number)
+        feminine_natural_number_name(fractional_order(number)) + " " + Declinations.name_with_declination(fractional_ten_powers_name(number), fractional_part)
+      end
+
+      def fractional_part(number)
         fractional_part = (number % 1).round(fractional_number_order(number))
-        feminine_natural_number_name(((fractional_part) * (10 ** fractional_number_order(number))).to_s.split('.')[0].to_i) + " " + Declinations.name_with_declination(fractional_ten_powers_name(number), fractional_part)
+        ((fractional_part) * (10 ** fractional_number_order(number))).to_s.split('.')[0].to_i
       end
 
       def whole_part_name(number)
