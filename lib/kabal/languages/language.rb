cd6abe4749da7885d92092c1509e2ced83800036
pavel <kalashnikovisme@gmail.com>
2014-05-10 04:04:23 +0400

    some refactoring

--- lib/kabal/languages/language.rb
+++ lib/kabal/languages/language.rb
@@ -2,7 +2,7 @@ module Kabal
   class Language
     def initialize
       @names = YamlLoader.yaml_object "languages/#{lang}"
-      @supports = YamlLoader.yaml_object "support"
+      @supports = YamlLoader.yaml_object("support")["support"][lang]
     end
 
     def lang
@@ -11,13 +11,13 @@ module Kabal
     end
 
     def supports_natural?
-      @supports["support"][lang]["natural"]
+      @supports["natural"]
     end
 
     #TODO реализовать метод supports_negative?
 
     def supports_fractional?
-      @supports["support"][lang]["fractional"]
+      @supports["fractional"]
     end
 
     def names
@@ -34,6 +34,17 @@ module Kabal
 
     def fractional?(number)
       number % 1 != 0
+    end
+
+    def natural_number_name(number)
+      #FIXME switch case next lines
+      return single(number) if number >= 0 and number <= 19
+      return two_words(number) if number >= 20 and number <= 99
+      return three_words(number) if number >= 100 and number <= 999
+      ten_powers(number) if number >= 1000
+    end
+    def natural?(number)
+      number % 1 == 0
     end
   end
 end
