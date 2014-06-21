9400247f9d17d974203946d61e8ed06e9f6f9f51
pavel <kalashnikovisme@gmail.com>
2014-06-21 16:32:05 +0400

    fix errors

--- lib/kabal/errors/no_language_support_error.rb
+++ lib/kabal/errors/no_language_support_error.rb
@@ -1,5 +1,9 @@
-class Kabal::Errors::NoLanguageSupportError < StandardError
-  def self.message
-    "There is no support of such language yet"
+module Kabal
+  module Errors
+    class NoLanguageSupportError < StandardError
+      def self.message
+        "There is no support of such language yet"
+      end
+    end
   end
 end
