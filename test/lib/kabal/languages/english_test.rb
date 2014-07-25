7d4123e5839dbfea936c2b0bb5e318a4333133dc
pavel <kalashnikovisme@gmail.com>
2014-07-25 23:10:40 +0400

    add test

--- test/lib/kabal/languages/english_test.rb
+++ test/lib/kabal/languages/english_test.rb
@@ -68,4 +68,8 @@ class EnglishTest < TestCase
     assert_equal "one point zero five", @eng.convert(1.05)
     assert_equal "minus one point zero five", @eng.convert(-1.05)
   end
+  def test_one_point_zero_zero_zero_zero_zero_zero_zero_five
+    assert_equal "one point zero zero zero zero zero zero zero five", @eng.convert(1.00000005)
+    assert_equal "minus one point zero zero zero zero zero zero zero five", @eng.convert(-1.00000005)
+  end
 end
