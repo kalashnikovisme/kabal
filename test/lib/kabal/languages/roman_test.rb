0ed666fdddf2c91e677c60be8b429cedbd2e9a92
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-22 00:43:15 +0400

    Update roman numbers

--- test/lib/kabal/languages/roman_test.rb
+++ test/lib/kabal/languages/roman_test.rb
@@ -4,4 +4,18 @@ class RomanTest < TestCase
   def setup
     @lang = Kabal::Roman.new
   end
+
+  def test_zero_number
+    assert_equal "0", @lang.convert(0)
+    assert_equal "0", @lang.convert(-0)
+  end
+  def test_to_text_with_single_number
+    assert_equal "I", @lang.convert(1)
+  end
+  def test_to_text_with_two_words_number
+    assert_equal "XXV", @lang.convert(25)
+  end
+  def test_to_text_with_three_words_number
+    assert_equal "DCXXV", @lang.convert(625)
+  end
 end
